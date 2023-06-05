bd60dbc0ec0e8d7ecf2133be1c1233e90e824efe
thaihaodev <thaihaodev199x@gmail.com>
2023-06-05 21:54:33 +0700

    commit 0506

--- src/components/TableUsers.js
+++ src/components/TableUsers.js
@@ -1,42 +1,48 @@
-import { useEffect } from 'react';
-import Table from 'react-bootstrap/Table';
+import { useEffect, useState } from "react";
+import Table from "react-bootstrap/Table";
+import { fetchAllUser } from "../services/UserService";
 
 const TableUsers = (props) => {
-    useEffect(()=>{
+  const [listUsers, setListUsers] = useState([]);
 
-    },[]);
+  useEffect(() => {
+    getUsers();
+  }, []);
 
-    return (
-    <Table striped bordered hover>
+  const getUsers = async () => {
+    let res = await fetchAllUser();
+    if (res && res.data) {
+      setListUsers(res.data);
+    }
+  };
+  return (
+    <>
+      <Table striped bordered hover>
       <thead>
         <tr>
-          <th>#</th>
+          <th>ID</th>
+          <th>Email</th>
           <th>First Name</th>
           <th>Last Name</th>
-          <th>Username</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td colSpan={2}>Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
+        {listUsers &&
+          listUsers.length > 0 &&
+          listUsers.map((item,index) => {
+            return (
+              <tr key={`index-${index}`}>
+                <td>{item.id}</td>
+                <td>{item.email}</td>
+                <td>{item.first_name}</td>
+                <td>{item.last_name}</td>
+              </tr>
+            );
+          })}
       </tbody>
     </Table>
+    </>
   );
-}
+};
 
 export default TableUsers;
